9bcc0499f4a05c37b7e306aa2d834507e04145ea
picasso250 <cumt.xiaochi@gmail.com>
2013-03-22 13:59:03 +0800

    update_status.py 结构化 ui实现基本的结构

--- ui.py
+++ ui.py
@@ -2,8 +2,16 @@
 from tkinter import *
 
 root = Tk()
+root.title('xz')
 
-w = Label(root, text="Hlellll")
-w.pack()
+status_var = StringVar()
+entry = Entry(root, textvariable=status_var)
+entry.pack(side=LEFT)
+
+def update_status():
+    status_text = status_var.get()
+
+button = Button(root, text='发布状态', command=update_status)
+button.pack(side=RIGHT)
 
 root.mainloop()
